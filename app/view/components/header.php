079f2a3e2b8fe9bd92f5a4391c478bd4e0412694
Yongjun Park <yopark@student.42seoul.kr>
2023-02-09 15:11:24 +0900

    Move: components & page/ -> view/

---
+++ app/view/components/header.php
@@ -0,0 +1,12 @@
+<header class="header">
+  <div class="header__container">
+    <h1><a href="/">CAMAGRU</a></h1>
+    <nav>
+      <ul>
+        <li><a href="/user/camera"><img src="asset/camera-icon.svg" /></a></li>
+        <li><a href="/user/settings"><img src="asset/gear-icon.svg" /></a></li>
+        <li><a href="/auth/logout"><img src="asset/logout-icon.svg" /></a></li>
+      </ul>
+    </nav>
+  </div>
+</header>
---
+++ app/view/page/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" href="assets/favicon.ico" />
+  <title>Camagru</title>
+  <link rel="stylesheet" href="style/global.css">
+  <link rel="stylesheet" href="style/layout.css">
+  <link rel="stylesheet" href="style/index.css">
+</head>
+<body>
+  <?php include 'view/components/header.php'; ?>
+</body>
+</html>
