8994f6997f8aab7b88dd53792133a6e0a6d741d1
Yongjun Park <yopark@student.42seoul.kr>
2023-02-10 08:00:54 +0900

    Init: Index

--- app/view/components/footer.php
+++ app/view/components/footer.php
@@ -1,3 +1,5 @@
 <footer class="footer">
-  <p>© Copyright by yopark in 42 Seoul. All rights reserved.</p>
+  <div class="footer__container">
+    <p>© Copyright by yopark in 42 Seoul. All rights reserved.</p>
+  </div>
 </footer>
--- app/view/page/index.php
+++ app/view/page/index.php
@@ -12,6 +12,92 @@
 </head>
 <body>
   <?php include 'view/components/header.php'; ?>
-  Index Page
+  <main>
+    <div class="board">
+      <div class="board__header">
+        <p>yopark</p>
+        <img src="asset/dot-menu-icon.svg" width="30px" />
+      </div>
+      <img class="board__image" src="https://picsum.photos/500/500" />
+      <div class="board__tab">
+        <img src="asset/heart-empty-icon.svg" />
+        <p class="board__likes">562 likes</p>
+      </div>
+      <div class="board__description">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, nobis cupiditate vel alias, natus animi id fugiat minima tempora soluta cumque vitae iste aut quos hic repudiandae ipsam in explicabo.</p>
+      </div>
+      <div class="board__comment-container">
+        <div class="board__comment">
+          <p class="board__comment__user">pikachu</p>
+          <p class="board__comment__text">Hello World!</p>
+        </div>
+        <div class="board__comment">
+          <p class="board__comment__user">pichu</p>
+          <p class="board__comment__text">My name is pichu :)</p>
+        </div>
+        <div class="board__comment">
+          <p class="board__comment__user">pikachu</p>
+          <p class="board__comment__text">Hello World!</p>
+        </div>
+      </div>
+    </div>
+    <div class="board">
+      <div class="board__header">
+        <p>yopark</p>
+        <img src="asset/dot-menu-icon.svg" width="30px" />
+      </div>
+      <img class="board__image" src="https://picsum.photos/500/500" />
+      <div class="board__tab">
+        <img src="asset/heart-empty-icon.svg" />
+        <p class="board__likes">562 likes</p>
+      </div>
+      <div class="board__description">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, nobis cupiditate vel alias, natus animi id fugiat minima tempora soluta cumque vitae iste aut quos hic repudiandae ipsam in explicabo.</p>
+      </div>
+      <div class="board__comment-container">
+        <div class="board__comment">
+          <p class="board__comment__user">pikachu</p>
+          <p class="board__comment__text">Hello World!</p>
+        </div>
+        <div class="board__comment">
+          <p class="board__comment__user">pichu</p>
+          <p class="board__comment__text">My name is pichu :)</p>
+        </div>
+        <div class="board__comment">
+          <p class="board__comment__user">pikachu</p>
+          <p class="board__comment__text">Hello World!</p>
+        </div>
+      </div>
+    </div>
+    <div class="board">
+      <div class="board__header">
+        <p>yopark</p>
+        <img src="asset/dot-menu-icon.svg" width="30px" />
+      </div>
+      <img class="board__image" src="https://picsum.photos/500/500" />
+      <div class="board__tab">
+        <img src="asset/heart-empty-icon.svg" />
+        <p class="board__likes">562 likes</p>
+      </div>
+      <div class="board__description">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, nobis cupiditate vel alias, natus animi id fugiat minima tempora soluta cumque vitae iste aut quos hic repudiandae ipsam in explicabo.</p>
+      </div>
+      <div class="board__comment-container">
+        <div class="board__comment">
+          <p class="board__comment__user">pikachu</p>
+          <p class="board__comment__text">Hello World!</p>
+        </div>
+        <div class="board__comment">
+          <p class="board__comment__user">pichu</p>
+          <p class="board__comment__text">My name is pichu :)</p>
+        </div>
+        <div class="board__comment">
+          <p class="board__comment__user">pikachu</p>
+          <p class="board__comment__text">Hello World!</p>
+        </div>
+      </div>
+    </div>
+
+  </main>
 </body>
 </html>
