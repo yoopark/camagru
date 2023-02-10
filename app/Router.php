45ef8a1992445705ab9ba7cb0275404f631d8dcf
Yongjun Park <yopark@student.42seoul.kr>
2023-02-10 14:24:07 +0900

    Revise: routing 내용 안으로 이동

--- app/Router.php
+++ app/Router.php
@@ -1,13 +1,52 @@
 <?php
 
+require_once 'Controller.php';
+
 class Router {
   private array $handlers;
+  private Controller $controller;
 
   private const METHOD_GET = 'GET';
   private const METHOD_POST = 'POST';
   private const METHOD_PATCH = 'PATCH';
   private const METHOD_PUT = 'PUT';
   private const METHOD_DELETE = 'DELETE';
+
+  function __construct(Controller $controller) {
+    $this->controller = $controller;
+
+    $this->get('/', 'view/page/index.php');
+    $this->get('/login', 'view/page/login.php');
+    $this->get('/signup', 'view/page/signup.php');
+    $this->get('/camera', 'view/page/camera.php');
+    $this->get('/settings', 'view/page/settings.php');
+    
+    $this->get('/api/user', function() {});
+    $this->post('/api/user', function() {});
+    $this->patch('/api/user', function() {});
+    $this->delete('/api/user', function() {});
+    $this->post('/api/login', function() { $this->controller->login(); });
+    $this->get('/api/logout', function() { $this->controller->logout(); }); // TODO: GET -> POST로 변경
+    
+    $this->get('/api/post/new', function() {});
+    $this->get('/api/post', function() {});
+    $this->post('/api/post', function() {});
+    $this->patch('/api/post', function() {});
+    $this->delete('/api/post', function() {});
+    
+    $this->get('/api/draft/me', function() {});
+    $this->post('/api/draft', function() {});
+    $this->delete('/api/draft', function() {});
+    
+    $this->get('/api/comment', function() {});
+    $this->post('/api/comment', function() {});
+    $this->patch('/api/comment', function() {});
+    $this->delete('/api/comment', function() {});
+    
+    $this->get('/api/like', function() {});
+    $this->post('/api/like', function() {});
+    $this->delete('/api/like', function() {});   
+  }
   
   function get(string $path, $handler) {
     $this->addHandler(self::METHOD_GET, $path, $handler);
